70f8a5125d89ff48ee6d0ebd94e43b26abafab4e
AdamEXu <contact@adamxu.net>
2024-08-12 16:27:40 -0700

    the time is now

---
+++ app.py
@@ -0,0 +1,99 @@
+import discord
+from discord.ext import commands
+import data_handler
+import datetime
+import random
+
+help_output = """
+## Sasbot Help
+This is Adam The Great's interpretation of Sasbot. It contains an economy, and some fun commands.
+
+**ECONOMY**
+Make money using `|work`
+Explore other jobs using `|job`
+Buy useful items in the shop with `|shop`
+Check how many coins you have with `|bal`
+See your inventory with `|inv`
+Use the bank using `|dep` and `|with`
+
+**FUN**
+View a random Sneaky Sasquatch meme with `|meme` ||If you would like to submit a meme, run `|submitmeme`.||
+
+There will be more commands in future versions!
+-# Version 0.1.0 (Test version)
+"""
+
+welcome_output = f"""
+You seem to be new here! I have ignored your previous command and have setup your profile with default values and a coin balance of 0.
+
+To start making coins, run `|work`.
+Keep an eye on your hunger bar! If it hits 0, there will be consequences. To fill it, run `|eat [food]`, where food is the name of the food you want to eat.
+To see what is in your inventory (and see what food you have to eat), run `|inv`.
+
+Take a look at `|help` for more information.
+"""
+
+intents = discord.Intents.all()
+
+client = commands.Bot(command_prefix='|', description="Test", intents=intents, help_command=None)
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+@client.command()
+async def work(ctx):
+  user_id = ctx.author.id
+  user_in_json = data_handler.ensure_user_in_json(user_id)
+  if user_in_json:
+    await ctx.reply(welcome_output)
+  else:
+    user_data = data_handler.get_user_data(user_id)
+    user_job = user_data['work_job']
+    last_work = user_data['last_work']
+    job_data = data_handler.get_job_data(user_job)
+    if last_work + job_data['cooldown'] > datetime.datetime.now().timestamp():
+      seconds = datetime.timedelta(seconds=last_work + job_data['cooldown'] - datetime.datetime.now().timestamp()).seconds
+      await ctx.reply(f"Please wait {seconds} seconds before working again.")
+    else:
+      earning_min = job_data['earning_min']
+      earning_max = job_data['earning_max']
+      earnings = random.randint(earning_min, earning_max)
+      user_data['last_work'] = datetime.datetime.now().timestamp()
+      user_data['coin_balance'] += earnings
+      user_data['earnings'] += earnings
+      data_handler.save_user_data(user_id, user_data)
+      await ctx.reply(f"You have earned {earnings} coins.")
+
+@client.command()
+async def bal(ctx):
+  user_id = ctx.author.id
+  user_in_json = data_handler.ensure_user_in_json(user_id)
+  if user_in_json:
+    await ctx.reply(welcome_output)
+  else:
+    user_data = data_handler.get_user_data(user_id)
+    balance = user_data['coin_balance']
+    bank_balance = user_data['bank_balance']
+    bank_lvl = user_data['bank_lvl']
+    user_publicname = ctx.author.display_name
+    if bank_lvl == 0:
+      bank_info = f"""**Bank**
+      You haven't unlocked the bank yet. See the `|shop` for more information."""
+    else:
+      bank_info = f"""**Bank**
+      Bank Balance: {bank_balance}
+      Bank Level: {bank_lvl}"""
+    embed = discord.Embed(title=f"{user_publicname}'s Balance", description=f"""**Coin Balance**
+{balance}
+
+{bank_info}""", color=0x00ff00)
+    await ctx.reply(embed=embed)
+
+
+
+@client.command()
+async def help(ctx):
+  await ctx.send(help_output)
+
+client.run('')
---
+++ data_handler.py
@@ -0,0 +1,64 @@
+import json
+def ensure_user_in_json(user_id):
+  with open('data.json', 'r') as f:
+    users = json.load(f)
+
+  in_users = False
+  for user in users:
+    if user_id == user['id']:
+      in_users = True
+      break
+
+  if not in_users:
+    users.append({
+      "id": user_id,
+      "coin_balance": 0,
+      "work_job": "campground",
+      "last_work": 0,
+      "vehicles": [],
+      "vehicle": "",
+      "hunger": 6,
+      "hunger_max": 6,
+      "bank_balance": 0,
+      "bank_lvl": 0,
+      "earnings": 0,
+      "shoe_lvl": 0
+    })
+    with open('data.json', 'w') as f:
+      json.dump(users, f)
+    return True
+  return False
+
+def get_user_data(user_id):
+  ensure_user_in_json(user_id)
+  with open('data.json', 'r') as f:
+    users = json.load(f)
+    for user in users:
+      if user_id == user['id']:
+        return user
+  return None
+
+def save_user_data(user_id, user_data):
+  with open('data.json', 'r') as f:
+    users = json.load(f)
+    for i in range(len(users)):
+      if user_id == users[i]['id']:
+        users[i] = user_data
+        break
+  with open('data.json', 'w') as f:
+    json.dump(users, f)
+
+def get_job_data(job):
+  with open('jobs.json', 'r') as f:
+    jobs = json.load(f)
+    for j in jobs:
+      if job == j['id']:
+        return j
+      
+def get_vehicle_data(vehicle):
+  with open('vehicles.json', 'r') as f:
+    vehicles = json.load(f)
+    for v in vehicles:
+      if vehicle == v['name']:
+        return v
+  return None
