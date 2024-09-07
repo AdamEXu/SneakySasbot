a4dffce3e511f91b1965a3f8d0e76b097cee1f39
AdamEXu <contact@adamxu.net>
2024-09-07 15:40:07 -0700

    DSFJSDF

--- app.py
+++ app.py
@@ -955,7 +955,7 @@ async def on_message(ctx):
         await ctx.reply("This bot will not work in DMs. Consider adding it to your server.", embed=bot_dm_embed)
         return
     print(ctx.guild)
-    if ctx.guild.id == 1275610612340097139:
+    if ctx.guild.id == 1275610612340097139 and ctx.author != 773996537414942763:
         embed = discord.Embed(title="Adobe Creative Cloud", description="This bot is not authorized to be used in this server. Please purchase an official license at [this link](https://adobe.com/creativecloud/plans.html) to continue.", color=0xff0000)
         embed.set_image(url="https://community.adobe.com/legacyfs/online/1117537_Screen%20Shot%202017-01-29%20at%208.44.09%20PM.png")
         await ctx.reply("Please purchase an official license at [this link](https://adobe.com/creativecloud/plans.html) to continue.", embed=embed)
